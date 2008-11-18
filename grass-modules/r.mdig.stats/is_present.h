d5a8935ff713ef8395738be64e551b9c916f08ce
Joel Pitt <joel@fruition-ubuntu>
2008-11-19 11:05:58 +1300

    Fix license messages for all files

--- grass-modules/r.mdig.stats/is_present.h
+++ grass-modules/r.mdig.stats/is_present.h
@@ -1,3 +1,22 @@
+/*
+ *  Copyright 2004      Bioprotection Centre, Lincoln University
+ *  Copyright 2006,2008 Joel Pitt, Fruition Technology
+ *
+ *  This file is part of Modular Dispersal In GIS.
+ *
+ *  Modular Dispersal In GIS is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
+ *
+ *  Modular Dispersal In GIS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ *  Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Modular Dispersal In GIS.  If not, see <http://www.gnu.org/licenses/>.
+ */
 /* Various streamlined functions for checking the presence of a cell,
  * depending on whether a null bitmap exists and what the raster cell
  * type is
--- mdig/mdig/Lifestage.py
+++ mdig/mdig/Lifestage.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python2.4
+#
+#  Copyright (C) 2006,2008 Joel Pitt, Fruition Technology
+#
+#  This file is part of Modular Dispersal In GIS.
+#
+#  Modular Dispersal In GIS is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or (at your
+#  option) any later version.
+#
+#  Modular Dispersal In GIS is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+#  Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with Modular Dispersal In GIS.  If not, see <http://www.gnu.org/licenses/>.
+#
 """ 
 Lifestage module. Part of MDiG - Modular Dispersal in GIS
 Copyright 2006, Joel Pitt
